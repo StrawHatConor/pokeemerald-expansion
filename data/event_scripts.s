5c258dc7626d44df59bcf3c80ce7d65e8e9ac805
StrawHatConor <conorcross188@gmail.com>
2026-02-12 22:21:28 +0000

    [#8] New starting town and lab

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1163,3 +1163,7 @@ EventScript_VsSeekerChargingDone::
 	.include "data/scripts/dexnav.inc"
 	.include "data/scripts/battle_frontier.inc"
 	.include "data/scripts/apricorn_tree.inc"
+
+	.include "data/maps/TwigtreeTown/scripts.inc"
+
+	.include "data/maps/TwigtreeTown_Lab/scripts.inc"
